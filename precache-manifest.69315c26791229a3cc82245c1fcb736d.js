a9fe2aa6cb479b3e4ba308990f2f2e0d9fc05a1f
hoan_nguyen <43391428+hoan_nguyen@users.noreply.github.com>
2020-06-24 13:46:14 -0500

    Updates

---
+++ precache-manifest.69315c26791229a3cc82245c1fcb736d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5e475848aaf3945a90a7e1b50dd4e4",
+    "url": "/hoan-react/index.html"
+  },
+  {
+    "revision": "fca420d7f2e972c8d6c4",
+    "url": "/hoan-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926064f36ae37a869614",
+    "url": "/hoan-react/static/js/2.296e00b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hoan-react/static/js/2.296e00b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca420d7f2e972c8d6c4",
+    "url": "/hoan-react/static/js/main.d4c51922.chunk.js"
+  },
+  {
+    "revision": "52814c40bf714d054bf1",
+    "url": "/hoan-react/static/js/runtime-main.fa4d9470.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hoan-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
